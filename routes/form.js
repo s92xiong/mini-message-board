10ecfabbad14de090b455862da274fca0d6995b1
Sheuh Xiong <sheuhxiong@gmail.com>
2021-06-26 13:57:23 -0400

    new commit

---
+++ routes/form.js
@@ -0,0 +1,14 @@
+// var express = require('express');
+// var router = express.Router();
+
+// // Add a router.get() for the "/new" route and point it to a template named "form".
+// router.get('/', function(req, res, next) {
+//   res.render('form');
+// });
+
+// // Send a POST request to the "/new" url specified by the action attribute
+// router.post("/", function(req, res, next) {
+  
+// });
+
+// module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,34 @@
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
 var express = require('express');
 var router = express.Router();
+const messages = require("../public/javascripts/messages");
 
-/* GET home page. */
+// GET home page for index
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Mini Message Board',
     messages: messages,
   });
+});
+
+// Add a router.get() for the "/new" route and point it to a template named "form".
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+// (1) Send a POST request to the "/new" url specified by the action attribute
+// (2) Access the contents of your form inside router.post() as an object called req.body
+// (3) The fields inside the req.body obj are named according to the name attribute on your inputs
+// (4) Take the contents of the form submission and push them into the messages array as an object
+// (5) Send users back to the index page after submitting a new message
+
+router.post("/new", function(req, res, next) {
+  // console.log(req.body);
+  messages.push({
+    text: req.body.messageText, 
+    user: req.body.messageUser, 
+    added: new Date()
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
